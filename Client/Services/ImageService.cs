bad3bf73ca95c6597f917852ca5efe3f42cd6a4b
Geraldsonp <Geraldson.a.P@gmail.com>
2023-03-23 19:41:45 -0400

    Adds Services

---
+++ Client/Services/ImageService.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Http.Json;
+using ImageUploader.Shared.Models;
+using ImageUploader.Shared.Models.Commands.Images;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace ImageUploader.Client.Services;
+
+public class ImageService : IImageService
+{
+    private string ApiAddress { get; set; } = "/api/Upload";
+    private readonly HttpClient _httpClient;
+
+    public ImageService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<UploadResult> UploadToBlobAsync(ImageUploadRequest request)
+    {
+        var response = await _httpClient.PostAsJsonAsync(ApiAddress, request);
+
+        var result = new UploadResult
+        {
+            IsSucess = response.IsSuccessStatusCode
+        };
+
+        if (response.IsSuccessStatusCode)
+        {
+            result.ImgUrl = await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            result.Error = await response.Content.ReadAsStringAsync();
+        }
+
+        return result;
+    }
+
+    public async Task<UploadResult> UploadAsync(ImageUploadRequest request)
+    {
+
+        var response = await _httpClient.PostAsJsonAsync(ApiAddress, request);
+
+        var result = new UploadResult
+        {
+            IsSucess = response.IsSuccessStatusCode
+        };
+
+        if (response.IsSuccessStatusCode)
+        {
+            result.ImgUrl = await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            result.Error = await response.Content.ReadAsStringAsync();
+        }
+
+        return result;
+    }
+}
