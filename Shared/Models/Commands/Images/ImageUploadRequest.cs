3bf33b74e7570b8ae3ed3e0321b4aeef1e8d4b73
Geraldsonp <Geraldson.a.P@gmail.com>
2023-03-23 19:34:38 -0400

    Adds models and components folder

---
+++ Shared/Models/Commands/Images/ImageUploadRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ImageUploader.Shared.Models.Commands.Images;
+
+public class ImageUploadRequest
+{
+    public Byte[] ImageBytes { get; set; }
+    public string ImgName { get; set; }
+}
