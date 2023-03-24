91f38169f1c86f433f1af8c169e51692935e5f5a
Geraldsonp <Geraldson.a.P@gmail.com>
2023-03-23 20:00:37 -0400

    Fixes References

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Blazored.Toast;
+using ImageUploader.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
